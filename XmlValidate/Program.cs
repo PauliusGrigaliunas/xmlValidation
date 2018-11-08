d1f84649a69825e6ab59aa04a4df53f9f447c4fb
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-11-09 00:35:20 +0200

    start

---
+++ XmlValidate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace XmlValidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("https://maitinimas.lt", @"C:\Users\Paulius\source\repos\XmlValidate\XmlValidate\XMLSchema.xsd");
+
+            XDocument xmlDocument = XDocument.Load(@"C:\Users\Paulius\source\repos\XmlValidate\XmlValidate\Food.xml");
+            bool validationErrors = false;
+
+            xmlDocument.Validate(schema, (s, e) =>
+            {
+                Console.WriteLine(e.Message);
+                validationErrors = true;
+            });
+
+            if (validationErrors)
+            {
+                Console.WriteLine("Validation failed");
+            }
+            else
+            {
+                Console.WriteLine("Validation succeeded");
+            }
+        }
+    }
+}
